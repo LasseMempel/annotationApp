6ac98c919efe6f40783fd2107d00a5988faf1da0
LasseMempel <lassemempel@gmail.com>
2024-12-13 10:13:22 +0100

    updates

---
+++ replace.py
@@ -0,0 +1,11 @@
+text = """
+Gürtelschnalle mit <Araldit> <getränkt>. Tauschierung mit <Busch-Schleifkörpern> und <Sandstrahlgebläse> <freigelegt>. Messingknöpfe auf der Gürtelplatte mit <Araldit> befestigt. Gürtelschnalle mit <gereinigt> und mit Schutzlack <Pantarol> überzogen. (3 Tage) 
+
+Wenzler
+Silberbleche abgenommen, Beschlag mit <Araldit> <getränkt>. Tauschierung mit <Busch-Schleifkörpern> und <Aaba-Gummischeibe> <freigelegt>. Silberbleche <Airbrasive> <gereinigt>, auf Plexiplatte mit <Mecosan> in der ursprünglichen Lage befestigt. Knöpfe <freigeschliffen> und <geschabt> alles mit <Pantarol-Schutzlack> überzogen. Beschlag: auf die aufgeraute Eisenoberfläche wurden Silberbleche eingeschlagen. Die Tauschierung besteht aus gekordelter Bronze, am äußeren Rand aus Messing. In den tauschierten bandförmigen Verzierungen sind mit Rundbolzen eingeschlagene Silberkügelchen.
+
+Fecht
+Zwei Schnallendorne mit <Araldit> <getränkt> und große Lücken mit ungefärbtem, versteiftem <Araldit> <ergänzt>. Die Oberfläche wurde mit <Schmirgelrädchen> und <Aaba> bearbeitet. Anschließend wurden die beiden Teile mit <Pantarol >überzogen.
+"""
+text = text.replace("<","").replace(">", "")
+print(text)
